6c05322166ca9af3fd7d88ceee0a60a8abc8a836
Khan <530037226@qq.com>
2022-03-09 16:59:46 +0000

    init

---
+++ src/everyday/platesBetweenCandles.java
@@ -0,0 +1,13 @@
+package everyday;
+
+/**
+ * @program: LeetCode
+ * @author: Khan
+ * @create: 2022-03-09 16:43
+ */
+public class platesBetweenCandles {
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+
+        return null;
+    }
+}
